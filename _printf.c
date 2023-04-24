596733fb3eaaa740b537e0ebfeaf05af06337de3
Elbadil <elbaliadil@gmai.com>
2023-04-24 12:50:28 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -25,9 +25,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			f = check_format(&format[i + 1]);
-			if (f == NULL)
-				return (-1);
-			else if (f != NULL)
+			if (f != NULL)
 			{
 				value = f(args);
 				count += value;
