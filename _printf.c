df8f004da362d4c0c009eedc1834ad538397023b
Elbadil <elbaliadil@gmai.com>
2023-04-24 13:30:57 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format == NULL || value < 0)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (format[i] != '\0')
 	{
